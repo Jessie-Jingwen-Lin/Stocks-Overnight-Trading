e1b226dc8bc522b9d558913627c840064ac87032
Jessie Lin <jessiejingwen@gmail.com>
2021-03-25 12:03:46 -0400

    Bug fix!!!

--- download_data.py
+++ download_data.py
@@ -30,13 +30,30 @@ import pickle
 
 def check_increased_gain(prices_morning, prices_afternoon):
     first_idx = prices_morning.first_valid_index()
-    last_idx = prices_afternoon.last_valid_index()
-    if (first_idx is None) or (last_idx is None):
-        return False
-    elif first_idx > last_idx: # This means that we downloaded some messed up data, so filter out this stock
+    last_idx_3 = prices_afternoon.last_valid_index()
+    last_idx_1030 = prices_morning.last_valid_index()
+
+    first_price = None
+    last_price = None
+    if first_idx is not None:
+        first_price = prices_morning[first_idx]
+
+    if (last_idx_3 is not None) and (last_idx_1030 is not None):
+        if last_idx_3 > last_idx_1030:
+            last_price = prices_afternoon[last_idx_3]
+        elif last_idx_3 < last_idx_1030:
+            last_price = prices_morning[last_idx_1030]
+        else: # (last_idx_3 == last_idx_1030)
+            last_price = prices_afternoon[last_idx_3]
+    elif (last_idx_3 is not None) and (last_idx_1030 is None):
+        last_price = prices_afternoon[last_idx_3]
+    elif (last_idx_3 is None) and (last_idx_1030 is not None):
+        last_price = prices_morning[last_idx_1030]
+
+    if (first_price is None) or (last_price is None):
         return False
     else:
-        return prices_morning[first_idx] < prices_afternoon[last_idx]
+        return first_price < last_price
 
 # Note: it must be that stock_price contains at least enough days of data for the days parameter
 def ranking(stock_price, start_day):
@@ -52,7 +69,7 @@ def ranking(stock_price, start_day):
     stats = pd.DataFrame(columns=['mean_profit_ratio', 'std_profit_ratio', 'sum_profit_ratio'])
 
     for ticker in filtered_tickers:
-        day_profit = (stock_price['15:00', ticker].iloc[1:].values - stock_price['10:30', ticker].iloc[:-1].values) / stock_price['10:30', ticker].iloc[:-1].values
+        day_profit = (stock_price['10:30', ticker].iloc[1:].values - stock_price['15:00', ticker].iloc[:-1].values) / stock_price['15:00', ticker].iloc[:-1].values
         num_non_nan = day_profit.shape[0] - np.isnan(day_profit).sum()
         if num_non_nan <= 1:
             # If we have only 0 or 1 day(s) of data, then filter it out, since we can't calculate STD
@@ -74,6 +91,8 @@ def main():
     tickers = stockdata.get_all_tickers()
     #tickers = tickers[:100]
     # print(tickers)
+
+    # tickers = ["AAPL", "DLPN"]
 
     eastern = timezone('US/Eastern')
     loc_dt = datetime.datetime.now(eastern)
